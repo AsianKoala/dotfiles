072c5b571f40c46bfe07adf36b2fdd381c7ba249
neil mehra <neilmehra@outlook.com>
2024-05-01 19:23:49 -0400

    added nvim submodule

--- polybar/generate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-counter=0
-i3-msg -t get_workspaces | tr ',' '\n' | sed -nr 's/"name":"([^"]+)"/\1/p' | while read -r name; do
-  printf 'ws-icon-%i = "%s;<insert-icon-here>"\n' $((counter++)) $name
-done
